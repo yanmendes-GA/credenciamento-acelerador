d381b6801b8ec327171f43271a73bfd9a55031ba
yanmendes-GA <yan.mendes@grupoacelerador.com.br>
2025-09-25 15:59:26 -0300

    Criando rotas

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,9 @@ import Dashboard from "./pages/Dashboard";
 import EventDetails from "./pages/EventDetails";
 import AccreditationDetails from "./pages/AccreditationDetails";
 import NotFound from "./pages/NotFound";
+import Step02 from "./pages/Step02";
+import Step03 from "./pages/Step03";
+import ThankYouPage from "./pages/ThankYouPage";
 
 const queryClient = new QueryClient();
 
@@ -24,29 +27,32 @@ const App = () => (
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/login" element={<Login />} />
-            <Route 
-              path="/dashboard" 
+            <Route path="/step02" element={<Step02 />} />
+            <Route path="/step03" element={<Step03 />} />
+            <Route path="/thank-you" element={<ThankYouPage />} />
+            <Route
+              path="/dashboard"
               element={
                 <ProtectedRoute>
                   <Dashboard />
                 </ProtectedRoute>
-              } 
+              }
             />
-            <Route 
-              path="/dashboard/events/:eventId" 
+            <Route
+              path="/dashboard/events/:eventId"
               element={
                 <ProtectedRoute>
                   <EventDetails />
                 </ProtectedRoute>
-              } 
+              }
             />
-            <Route 
-              path="/dashboard/accredited/:accreditedId" 
+            <Route
+              path="/dashboard/accredited/:accreditedId"
               element={
                 <ProtectedRoute>
                   <AccreditationDetails />
                 </ProtectedRoute>
-              } 
+              }
             />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
