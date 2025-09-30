5419809c01fa9952b5cc8b6691eef52a95ef14b2
Yan Mendes <cliente@172-0-20-15.lightspeed.iplsin.sbcglobal.net>
2025-09-30 16:41:43 -0300

    Ajustando o scroll das paginas

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,14 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { BrowserRouter } from "react-router-dom"
+import App from "./App"
+import ScrollToTop from "./components/ScrollToTop"
 
-createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <ScrollToTop />
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+)
